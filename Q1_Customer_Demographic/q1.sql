dbaf35e92894b1cf41f5725b3781ceea204cb211
shirleyhungwork <141710968+shirleyhungwork@users.noreply.github.com>
2023-09-09 06:11:11 +0800

    Update Q1

--- Q1_Customer_Demographic/q1.sql
+++ Q1_Customer_Demographic/q1.sql
@@ -1,11 +1,11 @@
 /*
-Q1: Create list of customer demographic (i.e. contact number, email address, name, total purchase price ytd)
+Q2: Create list of customer demographic (i.e. contact number, email address, name, total purchase price ytd, preference to receive receive e-mail promotions)
 Extract attributes from table Person.person, Person.EmailAddress, Person.Personphone, HumanResources.employee
 using column 'BusinessEntityId' as key
 */
 
 with customer as (
-	select p.BusinessEntityID, concat(p.FirstName, ' ', p.LastName) as PersonName, e.EmailAddress, pp.PhoneNumber,
+	select p.BusinessEntityID, concat(p.FirstName, ' ', p.LastName) as PersonName, p.EmailPromotion, e.EmailAddress, pp.PhoneNumber,
 	
 	--To extract total purchase year-to-date from column 'demographics' and convert data type from xml to decimal
 	(xpath('//IndividualSurvey:TotalPurchaseYTD/text()', p.demographics,
