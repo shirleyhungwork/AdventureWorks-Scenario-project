cfba6bc25a74d1b45d8ff0e0c0d7811affc6d123
shirleyhungwork <141710968+shirleyhungwork@users.noreply.github.com>
2023-09-09 06:36:10 +0800

    Update Q2

---
+++ Q2_Descriptive_Table/q2.sql
@@ -0,0 +1,32 @@
+/*
+Q2: Create descriptive table describing count of cusomters, maximum/minimum/average/standard deviation of total purchase YTD amount aggregated by their preference in email subscription:
+0 = Contact does not wish to receive e-mail promotions
+1 = Contact does wish to receive e-mail promotions from AdventureWorks
+2 = Contact does wish to receive e-mail promotions from AdventureWorks and selected partners
+
+Extract attributes from table Person.person, HumanResources.employee, Purchasing.Vendor
+using column 'BusinessEntityId' as key
+*/
+
+with customer as (
+	select p.EmailPromotion,
+	
+	--To extract total purchase year-to-date from column 'demographics' and convert data type from xml to decimal
+	(xpath('//IndividualSurvey:TotalPurchaseYTD/text()', p.demographics,
+		   ARRAY[ARRAY['IndividualSurvey', 'http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey']])::text[])[1]::Decimal(12,5) as TotalPurchaseYTD
+	
+	from person.person p 
+	left join humanresources.employee em
+	on p.businessentityid = em.businessentityid
+	left join Purchasing.Vendor v
+	on p.businessentityid = v.businessentityid
+	
+	--To exclude those that are employee / vendor 
+	where em.businessentityid is null and v.businessentityid is null
+) 
+	
+select emailpromotion, count(1) as CountOfPerson, 
+max(TotalPurchaseYTD) as MaxPurchaseYTD, min(TotalPurchaseYTD) as MinPurchaseYTD, 
+round(avg(TotalPurchaseYTD),4) as AvgPurchaseYTD, round(stddev(TotalPurchaseYTD),4) as STDPurchaseYTD
+from customer
+group by emailpromotion
