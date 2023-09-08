cd7298aecb961c213760e97cc841d64051b31a24
shirleyhungwork <141710968+shirleyhungwork@users.noreply.github.com>
2023-09-09 06:07:31 +0800

    Upload Query 1

---
+++ Q1_Customer_Demographic/q1.sql
@@ -0,0 +1,25 @@
+/*
+Q1: Create list of customer demographic (i.e. contact number, email address, name, total purchase price ytd)
+Extract attributes from table Person.person, Person.EmailAddress, Person.Personphone, HumanResources.employee
+using column 'BusinessEntityId' as key
+*/
+
+with customer as (
+	select p.BusinessEntityID, concat(p.FirstName, ' ', p.LastName) as PersonName, e.EmailAddress, pp.PhoneNumber,
+	
+	--To extract total purchase year-to-date from column 'demographics' and convert data type from xml to decimal
+	(xpath('//IndividualSurvey:TotalPurchaseYTD/text()', p.demographics,
+		   ARRAY[ARRAY['IndividualSurvey', 'http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey']])::text[])[1]::Decimal(12,5) as TotalPurchaseYTD
+	
+	from person.person p 
+	left join humanresources.employee em
+	on p.businessentityid = em.businessentityid
+	left join person.emailaddress e
+	on p.BusinessEntityID = e.BusinessEntityID
+	left join person.personphone pp
+	on p.BusinessEntityID = pp.BusinessEntityID
+	where em.loginid is null) 
+	
+select * from customer
+where TotalPurchaseYTD > 0
+order by TotalPurchaseYTD desc;
